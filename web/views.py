9fbff2bd78502d8a1ceb9b3bfecaa7194632bfaa
Porpathom <117138205+Porpathom@users.noreply.github.com>
2023-08-03 11:58:08 +0700

    c

--- web/views.py
+++ web/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render, HttpResponse
+from . import models
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    
+    context = {}
+    
+    students = models.Student.objects.all().order_by("id") 
+    
+    context['students'] = students
+    
+    # context = {
+    #     'students' : students
+    # }
+    
+    return render(request, 'index.html',context)
+
 
 def about(request):
     return render(request, 'about.html')
 
+
 def contact(request):
     return render(request, 'contact.html')
 
+def studentdetail(request, id):
+    context = {}
+    student = models.Student.objects.get(id=id)
+    context['student'] = student
+    return render(request, 'detail.html')
